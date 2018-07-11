8b02e26440f6d4cd89a2ba08050783193976ce1b
shenyan <52o@qq52o.cn>
2018-07-11 16:04:08 +0800

    qqdemo‘

--- test/qq/qqcallback.php
+++ test/qq/qqcallback.php
@@ -5,14 +5,15 @@
  */
 include_once './../../src/QqConnect.php';
 include_once './../../src/Common.php';
-use Auth\QqAuth;
+
+use Auth\QqConnect;
 
 //QQ互联管理中心 https://connect.qq.com/manage.html#/ 创建应用拿到 APP ID 和 APP Key
 $appId = '';
 $appKey = '';
 $callBackUrl = ''; // http://wiki.connect.qq.com/回调地址常见问题及修改方法
 
-$qqAuth = new QqAuth($appId,$appKey,$callBackUrl);
+$qqAuth = new QqConnect($appId,$appKey,$callBackUrl);
 
 $res = $qqAuth->qqCallBack();
 var_dump($res);
