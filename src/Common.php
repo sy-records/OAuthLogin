048f4b831b475aa9b86d2a10522ebb9de8208059
shenyan <52o@qq52o.cn>
2018-07-12 10:15:16 +0800

    新增一个postcurl

--- src/Common.php
+++ src/Common.php
@@ -16,8 +16,8 @@ class Common {
     public static function getContents($url){
         $ua='Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.9';
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_USERAGENT, $ua);
         curl_setopt($ch, CURLOPT_URL, $url);
         $response =  curl_exec($ch);
@@ -31,23 +31,54 @@ class Common {
     }
 
     /**
+     * 微博的接口需要post请求
+     * @param $url url链接
+     * @param $postData post数组
+     * @return mixed
+     */
+    public static function postContents($url,$postData=[]){
+        $ua='Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.9';
+        //初始化
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_USERAGENT, $ua);
+        //设置post方式提交
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $postData);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        //执行命令
+        $response = curl_exec($curl);
+        //关闭URL请求
+        curl_close($curl);
+        //显示获得的数据
+        //-------请求为空
+        if(empty($response)){
+            die("请求错误");
+        }
+        return $response;
+    }
+
+
+    /**
      * combineURL
      * 拼接url
      * @param string $baseURL   基于的url
      * @param array  $keysArr   参数列表数组
      * @return string           返回拼接的url
      */
-    public static function combineURL($baseURL,$keysArr){
-        $combined = $baseURL."?";
-        $valueArr = array();
+public static function combineURL($baseURL,$keysArr){
+    $combined = $baseURL."?";
+    $valueArr = array();
 
-        foreach($keysArr as $key => $val){
-            $valueArr[] = "$key=$val";
-        }
-
-        $keyStr = implode("&",$valueArr);
-        $combined .= ($keyStr);
-
-        return $combined;
+    foreach($keysArr as $key => $val){
+        $valueArr[] = "$key=$val";
     }
+
+    $keyStr = implode("&",$valueArr);
+    $combined .= ($keyStr);
+
+    return $combined;
+}
+
 }
